7c39d0ed9a5a4cf8dffbca2946bf5e215f7d751a
Kailee Smith <kaileesmith1501@gmail.com>
2021-02-08 20:59:19 -0800

    added folders and server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+// Dependencies
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+// LocalHost and port to deploy to Heroku
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(logger("dev"));
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// Database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// Routes
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
+  
