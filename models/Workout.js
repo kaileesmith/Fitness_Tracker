14d340c08a2ba8786e097fe4720ee9bcb9ff27d6
Kailee Smith <kaileesmith1501@gmail.com>
2021-02-08 21:10:34 -0800

    added to models-workout.js

---
+++ models/Workout.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    exercises: [
+        {
+            type: String,
+            name: String,
+            duration: Number,
+            weight: Number,
+            reps: Number,
+            sets: Number,
+            },
+        ],
+        date: {
+            type: Date,
+            default: Date.now
+        }
+        
+    });
+
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
