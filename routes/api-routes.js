7b33b35c0616642669d1d5f1db1450b9618fd78e
Kailee Smith <kaileesmith1501@gmail.com>
2021-02-08 21:56:36 -0800

    added start to api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,2 +1,23 @@
 const Workout = require("../models/Workout.js");
 
+// TO DO: recent workout, create workout and ranges for workout
+module.exports = function (app) {
+
+    //Create workout
+    app.post("/api/workouts", (req, res) => {
+		
+	});
+
+    // Create Exercise
+    app.put("/api/workouts", (req, res) => {
+		
+	});
+
+    // ALL Workouts
+    app.get("/api/workouts", (req, res) => {
+		Workout.find({})
+    
+	});
+
+
+    // Range
