ed6ef85a4b4e69b6c13cd3a816de1cb02d24d295
Kailee Smith <kaileesmith1501@gmail.com>
2021-02-17 20:07:20 -0800

    added get all workout capability

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,22 +2,36 @@ const Workout = require("../models/Workout.js");
 
 // // TO DO: recent workout, create workout and ranges for workout
 module.exports = function (app) {
-
-//     //Create workout
-    app.post("/api/workouts", (req, res) => {
-		Workout.create({})
+//     // ALL Workouts
+    app.get("/api/workouts", (req, res) => {
+		Workout.find({})
+        Workout.aggregate([
+            {
+                "$addFields": {
+                    "totalDuration": {
+                    "$sum": "$exercises.duration",
+                    },
+                },
+            },
+        ])
+        .sort({ date: -1 })
+        .then((result) => {
+            res.json(result);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
 });
+//     //Create workout
+//     app.post("/api/workouts", (req, res) => {
+// 		Workout.create({})
+// });
 
 //     // Create Exercise
-    app.put("/api/workouts/:id", (req, res) => {
+    // app.put("/api/workouts/:id", (req, res) => {
 		
-	});
+	// });
 
-//     // ALL Workouts
-//     app.get("/api/workouts", (req, res) => {
-// 		Workout.find({})
-    
-// 	});
 
 
     // Range
